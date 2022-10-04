75f01867f6b3b821672684ac0da63560a347bd18
vovkostril <edmond16913@gmail.com>
2022-10-04 16:34:31 +0300

    rewrite please netmiko base for dnwp and please create defs for hw monitoring + parse!

--- Libs/tueth.py
+++ Libs/tueth.py
@@ -3,16 +3,32 @@ import serial_ssh
 
 def sh_hw(comport):
     ret = None
+    ret2 = " 9   Ethernet   Ethernet"
+    sourceFile = open('demo.txt', 'w')
     print("----------------------------------------------------")
     set_cmd = ["do sh hw"]
     result = serial_ssh.send_command(comport, set_cmd)
-    for i in result:
-        print(result)
+    print(result, file=sourceFile)
+    sourceFile.close()
+    with open('demo.txt') as f:
+        # lines = f.readline()
+        # lines = f.readlines()
+        # [print(line) for line in f.readlines()]
+        for line in f.readlines():
+            if ret2 in line:
+                print("PASS")
+                ret = "PASS"
+            else:
+                ret = "NOT PASS"
     print("----------------------------------------------------")
+    return ret
 
+
+"""
     if 'Ethernet' in result:
         if 'Operational' in result:
             ret = "PASS"
     else:
         ret = "NOT PASS"
     return ret
+"""
