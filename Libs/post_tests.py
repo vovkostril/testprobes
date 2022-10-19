4651aa1ca0bd935b5099a3b6cc3b929bcc86fe32
vovkostril <edmond16913@gmail.com>
2022-10-19 14:59:11 +0300

    no card from standby

--- Libs/post_tests.py
+++ Libs/post_tests.py
@@ -2,11 +2,16 @@ import xml.etree.ElementTree as ET
 
 
 def xml_output_counter(xml_file):
+    result_pass = None
     tree = ET.parse(xml_file)
     root = tree.getroot()
     root1 = root[1]
-    print(root1)
-    # root = ET.fromstring
-    for x in root1.findall('tag'):
-        print(x.text)
-
+    for x in root1.iter():
+        if "Element 'stat' at" in str(x):
+            result = x.attrib
+            result_pass = result.get('pass')
+            if result.get('fail') == "0":
+                # print("PASS: " + result.get('pass'))
+                # print("FAIL: " + result.get('fail'))
+                break
+    return print(result_pass)
