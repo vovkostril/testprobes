0ce3d2f8cce84484f724ca7cd6fc4909219f61b3
vovkostril <edmond16913@gmail.com>
2022-11-09 10:19:07 +0200

    before main results

--- main.py
+++ main.py
@@ -17,9 +17,7 @@ if __name__ == '__main__':
     pp = None
     
     p = r"robot -d Results .\Tests\iteration_test.robot"
-    # p = r"robot -d Results .\Tests\bolvanka.robot"
 
-    # result2 = "C:/Users/anastasiia/PycharmProjects/TestResults/6612-latest-2022-11-3.zip"
     # result3 = zip_move_scp.scp_scp(result2, "anastasiia", ip="192.168.10.31", password=getpass(), remote_folder="/home/anastasiia")
 
     origin_dir  = "C:/Users/anastasiia/.vscode/tri/tributary/"
@@ -37,20 +35,21 @@ if __name__ == '__main__':
     # start testing
 
     if pp:
-        # p = os.system(r"robot -d Results .\Tests\power_cards.robot")
-        # p = os.system(r"robot -d Results .\Tests\power_cards_hw_cards.robot")
         p = os.system(r"robot -d Results .\Tests\bolvanka.robot")
         print(p)
 
         result_from = post_tests.xml_output_counter(r".\Results\output.xml")
         result1 = zip_move_scp.move_move(parent_dir, origin_dir)
-        # print(result1)
         # sleep(5)
         # result2 = zip_move_scp.zip_zip(result1, dst_path_copy)
         # scp!
         # result3 = zip_move_scp.scp_scp(result2, "anastasiia", ip="192.168.10.31", password=getpass(), remote_folder="/home/anastasiia")
 
 
+
+
+
+    # have tried the robot framework api for parse result - unsuccesful 
     # result = ExecutionResult(dest_folder + 'output.xml')
     # result.configure(stat_config={'suite_stat_level': 2,
     #                               'tag_stat_combine': 'tagANDanother'})
